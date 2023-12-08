ac4e1b83a500cdb07ec2daf2053a75b1eb4eacd1
HOLYPERSON <31584910+omy181@users.noreply.github.com>
2023-12-08 23:53:07 +0300

    Calculations and normalization

--- K_means.py
+++ K_means.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
 import math as math
+import pprint as pp
 
 def FindClosestCentroid(centroids,point):
     closest_distance = 0
-    closest_centroid = None
+    closest_centroid_index = 0
 
-    for centroid in centroids:
+    for i,centroid in enumerate(centroids):
         #calculate distances
         varsum = 0
         for c_var,p_var in zip(centroid,point):
@@ -16,21 +17,50 @@ def FindClosestCentroid(centroids,point):
 
         if(closest_distance == 0 or distance < closest_distance):
             closest_distance = distance
-            closest_centroid = centroid
+            closest_centroid_index = i
 
-    return closest_centroid
+    return closest_centroid_index
+
+def AssigntoNearestCentroid(centroids,points):
+    Point_Clusters = {}
+    for i in range(len(centroids)): 
+        Point_Clusters[i] = []
+
+    for point in points:
+        Closest_Centroid_index = FindClosestCentroid(centroids,point)
+        Point_Clusters[Closest_Centroid_index].append(point)
+    
+    return Point_Clusters
+
+def FindNewCentroids(Point_Clusters):
+    centroids = []
+    for Cluster_Index,Cluster in zip(Point_Clusters,Point_Clusters.values()):
+        Sum = 0
+        for point in Cluster:
+            Sum += point
+        Sum /= len(Cluster)
+        centroids.append(Sum)
+
+
+    return centroids
 
 def CalculateKmeans(data:pd.DataFrame,k,epoch):
     centroids = data.sample(k).to_numpy()   
     points = data.to_numpy()
 
-    Point_Clusters = {}
+    Point_Clusters = None
 
-    for point in points:
-        Closest_Centroid = FindClosestCentroid(centroids,point)
-        Point_Clusters[point] = Closest_Centroid #dictionary index is not good find a better way to put index to the points
 
-    print(Point_Clusters)
+    for e in range(epoch):
+        Point_Clusters = AssigntoNearestCentroid(centroids,points)
+        centroids = FindNewCentroids(Point_Clusters)
 
-    return 
+    return centroids
 
+def AskQuestion(Centroids,Question_Point):
+    i = FindClosestCentroid(Centroids,Question_Point)
+    print("\n\nGiven Point is: ",Question_Point)
+    print("Closest Centroid is: ",Centroids[i])
+
+def NormalizeData(df:pd.DataFrame):
+    return (df-df.min())/(df.max()-df.min())
--- Test.py
+++ Test.py
@@ -1,6 +1,13 @@
 from K_means import *
 from pandas import read_csv
+import numpy as np
 
 data = read_csv("midtermProject-part2-data.csv")
+data = NormalizeData(data)
 
-CalculateKmeans(data,k=3,epoch=1)
+
+Centroids = CalculateKmeans(data,k=3,epoch=2)
+
+
+
+#AskQuestion(Centroids,data.to_numpy()[0])
